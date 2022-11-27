6fea2fcc2b4e9429ea90f7eee7034452199eb6eb
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-11-27 21:55:02 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = 'BOB'
+}
+//upperCaseCustomerName()
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+
+const leastFavoriteCustomer = "Assignment to constant variable."
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "is bob"
+    return leastFavoriteCustomer
+}
